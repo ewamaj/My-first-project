a3a4fead7cc374ddc4e724c669044bf1e41c4625
Ewa <ewa.majcherek@op.pl>
2017-11-29 01:06:03 +0100

    first commit

---
+++ app/src/main/java/com/example/android/christmas_card/christmas_main.java
@@ -0,0 +1,13 @@
+package com.example.android.christmas_card;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class christmas_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_christmas_main);
+    }
+}
